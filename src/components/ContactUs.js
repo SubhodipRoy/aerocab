8ce35187150b1efa89a81248b948df9621366145
SubhodipRoy161101 <subhodiproy161101@gmail.com>
2022-10-16 18:27:49 +0530

    Updated Contact Page

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -11,32 +11,31 @@ import {
   Icon,
 } from "@chakra-ui/react";
 
+import "./ChardhamStyles.css";
+
+import { FiPhoneCall, FiMap } from "react-icons/fi";
+
 const ContactUs = () => {
   return (
     <div>
-      <Container>
+      <Container className="AccordionButton">
         <Accordion allowMultiple my={12}>
           <AccordionItem bg="blue.100" borderRadius="50" p={4} m={2}>
             <h2>
               <AccordionButton>
                 <Box flex="1" textAlign="left">
                   <Heading as="h2" size="md">
-                    {/* <Icon as={BiBook} w={10} h={10} p={1} my={10} /> */}
-                    <span>Address</span>
+                    <Icon as={FiPhoneCall} w={10} h={10} p={1} my={10} />
+                    <span>Phone Number</span>
                   </Heading>
                 </Box>
                 <AccordionIcon />
               </AccordionButton>
             </h2>
             <AccordionPanel pb={4}>
-              <b>MAP: </b>₹5500 &emsp;<b>CP:</b>₹4500
+              <b>Phone 1: </b> <a href="tel:+917248486092">+91 72484 86092</a>
               <br />
-              <b>Includes:</b>
-              <br />
-              <b>MAP:</b>Double Occupancy, Breakfast, Dinner, TV, Gyser,
-              Attached Bathroom, Garden
-              <br />
-              <b>MAP:</b>Double Occupancy, TV, Gyser, Attached Bathroom, Garden
+              <b>Phone 2: </b> <a href="tel:+919105947108">+91 72484 86092</a>
             </AccordionPanel>
           </AccordionItem>
         </Accordion>
@@ -46,22 +45,22 @@ const ContactUs = () => {
               <AccordionButton>
                 <Box flex="1" textAlign="left">
                   <Heading as="h2" size="md">
-                    {/* <Icon as={BiBook} w={10} h={10} p={1} my={10} /> */}
-                    <span>Phone Numbers</span>
+                    <Icon as={FiMap} w={10} h={10} p={1} my={10} />
+                    <span>Address</span>
                   </Heading>
                 </Box>
                 <AccordionIcon />
               </AccordionButton>
             </h2>
             <AccordionPanel pb={4}>
-              <b>MAP: </b>₹5500 &emsp;<b>CP:</b>₹4500
-              <br />
-              <b>Includes:</b>
-              <br />
-              <b>MAP:</b>Double Occupancy, Breakfast, Dinner, TV, Gyser,
-              Attached Bathroom, Garden
-              <br />
-              <b>MAP:</b>Double Occupancy, TV, Gyser, Attached Bathroom, Garden
+              <p>
+                112, Near Welcome Resturant,
+                <br />
+                Nainital Road <br />
+                Pantnagar <br />
+                Uttarakhand <br />
+                263149
+              </p>
             </AccordionPanel>
           </AccordionItem>
         </Accordion>
